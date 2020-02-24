d560851067daaefe7a3cc3244b96fefa7f7c40f4
Robin Anderson <robin@rbanderson.co.uk>
2020-02-24 13:07:13 +0000

    add ai runner console app

---
+++ Splendor.ConsoleRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿using Splendor.Core.AI;
+using System;
+
+namespace Splendor.ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aiPlayers = new[]
+            {
+                new StupidSplendorAi("Robin"),
+                new StupidSplendorAi("James"),
+                new StupidSplendorAi("Mat"),
+            };
+
+            var runner = new AiGameRunner(aiPlayers, Console.WriteLine);
+
+            runner.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
