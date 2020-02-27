9f889a130c629e0cfafd91cd059ee69c9f3b6b09
Robin Anderson <robin@rbanderson.co.uk>
2020-02-27 13:29:05 +0000

    run 1000 games

--- Splendor.ConsoleRunner/Program.cs
+++ Splendor.ConsoleRunner/Program.cs
@@ -1,11 +1,18 @@
 ﻿using Splendor.Core.AI;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Splendor.ConsoleRunner
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            Run1000();
+        }
+
+        static void Run1()
         {
             var aiPlayers = new[]
             {
@@ -18,6 +25,34 @@ namespace Splendor.ConsoleRunner
 
             runner.Run();
 
+            Console.ReadLine();
+        }
+
+        static void Run1000()
+        {
+            var scoreBoard = new Dictionary<ISpendorAi, int> 
+            {
+                { new StupidSplendorAi("Robin"), 0},
+                { new StupidSplendorAi("James"), 0},
+                { new StupidSplendorAi("Mat"), 0},
+            };
+
+            for(int i = 0; i < 1000; i++)
+            {
+                if (i % 100 == 0) Console.Write(".");
+                var runner = new AiGameRunner(scoreBoard.Select(p=>p.Key), _ => { });
+                var results = runner.Run();
+                var max = results.Values.Max();
+                foreach(var ai in results.Where(r=>r.Value == max)) scoreBoard[ai.Key]++;
+            }
+
+            Console.Write(Environment.NewLine);
+
+            foreach(var row in scoreBoard)
+            {
+                Console.WriteLine($"{row.Key.Name} {row.Value} wins");
+            }
+            
             Console.ReadLine();
         }
     }
