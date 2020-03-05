b7ff81a9c444856b49eae84dabd183a503d09286
Robin Anderson <robin@rbanderson.co.uk>
2020-03-05 15:37:22 +0000

    Update Pool.cs

--- Splendor.Core/Domain/Pool.cs
+++ Splendor.Core/Domain/Pool.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 
-namespace Splendor.Core.Domain
+namespace Splendor.Core
 {
     public interface IPool
     {
         int this[TokenColour index] { get; }
         Pool Clone();
+        Pool DeficitFor(IPool other);
+        Pool MergeWith(IPool other);
+        int Gold { get;}
+        int White { get; }
+        int Blue { get; }
+        int Red { get; }
+        int Green { get; }
+        int Black { get; }
     }
 
     public sealed class Pool : IPool
@@ -18,6 +26,10 @@ namespace Splendor.Core.Domain
             Red = red;
             Green = green;
             Black = black;
+        }
+
+        public Pool()
+        {
         }
 
         public int Gold { get; private set; }
@@ -70,6 +82,35 @@ namespace Splendor.Core.Domain
         public Pool Clone()
         {
             return new Pool(Gold, White, Blue, Red, Green, Black);
+        }
+
+        public Pool MergeWith(IPool other)
+        {
+            return new Pool(
+                Gold + other.Gold,
+                White + other.White,
+                Blue + other.Blue,
+                Red + other.Red,
+                Green + other.Green,
+                Black + other.Black);
+        }
+
+        public Pool DeficitFor(IPool other)
+        {
+            var gold  = other.Gold  - Gold;  gold  = gold  < 0 ? 0 : gold;
+            var white = other.White - White; white = white < 0 ? 0 : white;
+            var blue  = other.Blue  - Blue;  blue  = blue  < 0 ? 0 : blue;
+            var red   = other.Red   - Red;   red   = red   < 0 ? 0 : red;
+            var green = other.Green - Green; green = green < 0 ? 0 : green;
+            var black = other.Black - Black; black = black < 0 ? 0 : black;
+
+            return new Pool(
+                gold,
+                white,
+                blue,
+                red,
+                green,
+                black);
         }
     }
 }
