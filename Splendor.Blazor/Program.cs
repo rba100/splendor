b243a402467491f411b108f9f033fab87499c5d4
matburton <matburton@gmail.com>
2020-03-09 13:43:31 +0000

    Added empty client-side Blazor site
    Shows nothing but `Loaded`, but has a cool spinner

---
+++ Splendor.Blazor/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Splendor.Blazor
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                                 .UseBlazorStartup<Startup>()
+                                 .Build()
+                                 .Run();
+        }
+
+        public sealed class Startup
+        {
+            public void Configure(IComponentsApplicationBuilder app)
+            {
+                app.AddComponent<App>("app");
+            }
+
+            public void ConfigureServices(IServiceCollection services) {}
+        }
+    }
+}
