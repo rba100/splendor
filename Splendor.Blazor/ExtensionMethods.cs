153c3fe9720d28809185583895c9dfbb4d6ba090
matburton <matburton@gmail.com>
2020-03-10 17:56:20 +0000

    Removed Honeycomb and fixed models a bit

--- Splendor.Blazor/ExtensionMethods.cs
+++ Splendor.Blazor/ExtensionMethods.cs
@@ -7,21 +7,27 @@ namespace Splendor.Blazor
     {
         public static string CardColour(this TokenColour c) => c switch
         {
-            White => "background-color--grey--3",
-            Red   => "background-color--red--6",
-            Blue  => "background-color--blue--5",
-            Green => "background-color--green--5",
-            _     => "background-color--black"
+            White => "#dcdcdc",
+            Red   => "#a30000",
+            Blue  => "#3c85df",
+            Green => "#1aac1e",
+            _     => "black"
         };
 
         public static string TokenColour(this TokenColour c) => c switch
         {
-            Gold  => "background-color--orange--5",
-            White => "background-color--white",
-            Red   => "background-color--red--5",
-            Blue  => "background-color--blue--4",
-            Green => "background-color--green--4",
-            _     => "background-color--grey--9"
+            Gold  => "#fc9003",
+            White => "white",
+            Red   => "#c00",
+            Blue  => "#67a9f1",
+            Green => "#4abc4b",
+            _     => "#222"
+        };
+
+        public static string TextColour(this TokenColour c) => c switch
+        {
+            Black => "white",
+            _     => "black"
         };
     }
 }
