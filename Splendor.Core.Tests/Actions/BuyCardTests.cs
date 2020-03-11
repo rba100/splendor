de6fcc1c3cef6a2f60746c2f1799586712035300
Robin Anderson <robin@rbanderson.co.uk>
2020-03-11 17:27:52 +0000

    unit test for Mat's case

--- Splendor.Core.Tests/Actions/BuyCardTests.cs
+++ Splendor.Core.Tests/Actions/BuyCardTests.cs
@@ -1,11 +1,14 @@
 using NUnit.Framework;
 using Splendor.Core.Actions;
+using System.Linq;
 
 namespace Splendor.Core.Tests.Actions
 {
     [TestFixture]
     public class BuyCardTests
     {
+        private static GameState DefaultGame = new DefaultGameInitialiser(new DefaultCards()).Create(new[] { "a", "b", "c" });
+
         [TestCase(true, TestName = "CanAffordCard uses gold via Player")]
         [TestCase(false, TestName = "CanAffordCard uses gold via Pool")]
         public void CanAffordCard_uses_gold(bool playerVarient)
@@ -62,6 +65,28 @@ namespace Splendor.Core.Tests.Actions
             else payment = BuyCard.CreateDefaultPaymentOrNull(budget, card);
 
             Assert.Null(payment);
+        }
+
+        [Test]
+        public void BuyCard_can_use_bonus()
+        {
+            var card = new Card(1, 0, new Pool(0, 0, 2, 0, 2, 1), TokenColour.Black);
+            var cardInPlay = new Card(1, 0, new Pool(), TokenColour.Green);
+            var purse = new Pool(0, 1, 2, 0, 1, 1);
+
+            var player = DefaultGame.CurrentPlayer
+                .Clone(withPurse: purse, withCardsInPlay: new []{ cardInPlay });
+
+            var tiers = DefaultGame.Tiers.ToList();
+            tiers.RemoveAll(r => r.Tier == 1);
+            tiers.Add(new BoardTier(1, new[] { card }, 1));
+
+            var gameState = DefaultGame.CloneWithPlayerReplacedByName(player).Clone(withTiers: tiers);
+
+            var payment = BuyCard.CreateDefaultPaymentOrNull(player, card);
+            Assert.AreEqual(4, payment.Sum);
+            var action = new BuyCard(card, payment);
+            var nextState = action.Execute(gameState);
         }
 
         private static bool PoolsAreExactlyEquivilent(IPool a, IPool b)
