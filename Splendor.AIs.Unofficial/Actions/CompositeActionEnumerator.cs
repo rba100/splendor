4f339e125eb762268c32182bfa187f6576b1a327
matburton <matburton@gmail.com>
2020-03-13 00:30:08 +0000

    Started ground work for new AIs
    Only going to make a Bogo AI first, but hoping I can then use these classes to build a better AI

---
+++ Splendor.AIs.Unofficial/Actions/CompositeActionEnumerator.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Splendor.Core;
+using Splendor.Core.Actions;
+
+namespace Splendor.AIs.Unofficial.Actions
+{
+    /// <remarks>Returns the groups from all underlying
+    ///          enumerators. Does not combine groups</remarks>
+    ///
+    public sealed class CompositeActionEnumerator : IActionEnumerator
+    {
+        public CompositeActionEnumerator
+            (params IActionEnumerator[] underlyingEnumerators)
+        {
+            m_UnderlyingEnumerators = underlyingEnumerators?.ToArray()
+                ?? throw new ArgumentNullException(nameof(underlyingEnumerators));
+
+            if (m_UnderlyingEnumerators.Contains(null))
+            {
+                throw new ArgumentException("Contained null",
+                                            nameof(underlyingEnumerators));
+            }
+        }
+
+        public IEnumerable<IEnumerable<IAction>> GenerateValidActionVariations
+            (GameState gameState)
+        {
+            return m_UnderlyingEnumerators.SelectMany
+                (e => e.GenerateValidActionVariations(gameState));
+        }
+
+        private readonly IReadOnlyCollection<IActionEnumerator>
+            m_UnderlyingEnumerators;
+    }
+}
---
+++ Splendor.AIs.Unofficial/BogoSpendorAi.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+using Splendor.AIs.Unofficial.Actions;
+using Splendor.Core;
+using Splendor.Core.Actions;
+using Splendor.Core.AI;
+
+namespace Splendor.AIs.Unofficial
+{
+    /// <summary>Picks a random valid action variation
+    ///          from a random group of valid actions</summary>
+    ///
+    public sealed class BogoSpendorAi : ISpendorAi
+    {
+        public string Name => nameof(BogoSpendorAi);
+
+        /// <remarks>Instances of Random must not be shared
+        ///          if there is any chance of concurrency</remarks>
+        ///
+        public Random Random { private get; set; } = new Random();
+
+        public IActionEnumerator ActionEnumerator { private get; set; }
+
+        public IAction ChooseAction(GameState gameState) =>
+            ActionEnumerator.GenerateValidActionVariations(gameState)
+                            .GetRandomItem(Random)
+                            .GetRandomItem(Random);
+    }
+}
