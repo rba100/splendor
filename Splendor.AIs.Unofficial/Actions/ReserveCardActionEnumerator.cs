9855c1b38464f9cc58872fb87f696150a2428e9c
matburton <matburton@gmail.com>
2020-03-16 14:04:53 +0000

    Bogo AI will now reserve cards
    But it never buys any cards

--- Splendor.AIs.Unofficial/Actions/ReserveCardActionEnumerator.cs
+++ Splendor.AIs.Unofficial/Actions/ReserveCardActionEnumerator.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Splendor.Core;
 using Splendor.Core.Actions;
@@ -19,10 +20,46 @@ namespace Splendor.AIs.Unofficial.Actions
 
             if (gameState.CurrentPlayer.ReservedCards.Count >= 3)
             {
-                return new IEnumerable<IAction>[] {};
+                yield break;
             }
 
-            return new IEnumerable<IAction>[] {};
+            var tokenColours = gameState.CurrentPlayer.Purse.Sum <= 9 ? null
+                             : gameState.CurrentPlayer.Purse.Colours()
+                                        .Union(new [] { TokenColour.Gold })
+                                        .ToArray();
+
+            var tiers = gameState.Tiers.Where(t => t.HasFaceDownCardsRemaining);
+
+            foreach (var tier in tiers)
+            {
+                yield return WithReturnColours
+                    (tokenColours, c => new ReserveFaceDownCard(tier.Tier, c));
+            }
+
+            var cards = gameState.Tiers.SelectMany(t => t.ColumnSlots.Values);
+
+            foreach (var card in cards.Where(c => c != null))
+            {
+                yield return WithReturnColours
+                    (tokenColours, c => new ReserveCard(card, c));
+            }
+        }
+
+        private static IEnumerable<IAction> WithReturnColours
+            (IEnumerable<TokenColour> colours,
+             Func<TokenColour?, IAction> toAction)
+        {
+            if (colours is null)
+            {
+                yield return toAction(null);
+
+                yield break;
+            }
+
+            foreach (var colour in colours)
+            {
+                yield return toAction(colour);
+            }
         }
     }
 }
